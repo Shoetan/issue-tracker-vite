7059b81f9d9abd2ab8b5f102aa2a09d42649114a
Emmanuel <emmysoetan@gmail.com>
2022-04-11 23:43:47 +0100

    created backend server

--- backend.js
+++ backend.js
@@ -1,1 +1,10 @@
+
+import express from 'express'
+import cors from 'cors'
+require('dotenv'),config()
+
 const PORT = 8080
+
+const app = express()
+
+app.listen(PORT, () => { console.log('listening on port: ${PORT}')})
