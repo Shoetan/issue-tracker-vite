6f04a1d8a4f69bbcb525a717c95907a40a7c60f0
unknown <emmysoetan@gmail.com>
2022-04-22 01:13:33 +0100

    Successfully routed both login and sign up tot the dasboard

--- src/components/registerpage.jsx
+++ src/components/registerpage.jsx
@@ -1,7 +1,11 @@
 import Navbar from './navbar'
 import Form from './form'
-import { registerUser } from '../Logic/register'
+
 import {useNavigate} from 'react-router-dom'
+
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+
+import { auth } from '../firebase_config'
 
 
 const RegisterPage = () => {
@@ -14,6 +18,22 @@ const RegisterPage = () => {
     /* this function will run when the button in the navbar and will redirect the user to the login page */
     const click = () => {
         navigate('/')
+    }
+
+
+    const registerUser = async (email, password) => { 
+    
+        try {
+            
+            await createUserWithEmailAndPassword(auth, email, password)
+            console.log('I am registered')
+            navigate('/dashboard')
+    
+        } catch (error) {
+           console.log(error.message) 
+        }
+       
+        
     }
 
 
@@ -36,7 +56,7 @@ const RegisterPage = () => {
                 btntext="Sign up"
 
                 /*below is the function used to login in an already existing user it accepts a function as its value*/
-                fn={registerUser}
+                handleClick={registerUser}
                 
             />
 
