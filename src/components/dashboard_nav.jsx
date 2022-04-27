b9db71defee12477ef724497912076b2998f55a9
unknown <emmysoetan@gmail.com>
2022-04-27 21:29:10 +0100

    made button on the navbar of the dashboard more visible by adding some shadow

--- src/components/dashboard_nav.jsx
+++ src/components/dashboard_nav.jsx
@@ -9,13 +9,13 @@ const DashboardNav = () => {
     <div className="flex  bg-secondary-color p-2 justify-end items-center gap-6 pr-6 sticky top-0">
 
       {/* first the navigation should span the whole page */}
-      <div className="flex bg-primary-color items-center justify-center rounded-md p-1 shadow-lg shadow-dark-color">
+      <div className="flex bg-primary-color items-center justify-center rounded-md p-1 shadow-lg shadow-black-color">
         <BiSearchAlt size={25} color="#767676"/>
         <input type="text"  className=" bg-primary-color outline-none"/>
       </div>
 
       <div>
-        <button className="font-poppins bg-accent-color text-white-color hover:bg-primary-color hover:text-black-color py-2 px-6  rounded-full shadow-lg shadow-dark-color">Add issue</button>
+        <button className="font-poppins bg-accent-color text-white-color hover:bg-primary-color hover:text-accent-color py-2 px-6  rounded-full shadow-lg shadow-black-color">Add issue</button>
       </div>
     </div>
     <div>
